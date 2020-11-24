3618d2d453d05f667b5c45408937ad2915409e64
Helton Oliveira <helton.bjj@gmail.com>
2020-11-24 00:24:06 -0500

    Adding JWT tokenization

---
+++ src/main/java/com/heltondev/manager/entity/Jwt.java
@@ -0,0 +1,31 @@
+package com.heltondev.manager.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.io.Serializable;
+
+public class Jwt implements Serializable{
+
+	@Data
+	public static class JwtRequestBuilder implements Serializable {
+
+		private static final long serialVersionUID = 5926468583005150707L;
+
+		@NonNull private String username;
+		@NonNull private String password;
+
+		public JwtRequestBuilder(String username, String password) {
+			setUsername(username);
+			setPassword(password);
+		}
+	}
+
+	@Data
+	public static class JwtResponseBuilder implements Serializable {
+		private static final long serialVersionUID = -8091879091924046844L;
+		private final String token;
+	}
+}
+
+
